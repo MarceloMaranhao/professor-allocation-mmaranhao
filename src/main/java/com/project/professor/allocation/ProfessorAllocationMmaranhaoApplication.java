a620e395439f730934592b306bb49121e4b0e6c4
Marcelo Maranhão <marcelo.maranhao@gmail.com>
2022-08-17 20:56:37 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationMmaranhaoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationMmaranhaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationMmaranhaoApplication.class, args);
+	}
+
+}
